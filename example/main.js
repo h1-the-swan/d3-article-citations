3724476cd5cd2222fb88d6e0d37887de539c4032
h1-the-swan <jason.portenoy@gmail.com>
2019-04-07 17:53:59 -0700

    refactor as es6 class

--- example/main.js
+++ example/main.js
@@ -2,5 +2,6 @@ var NodelinkVisTimeDirected = NodelinkVisTimeDirected.NodelinkVisTimeDirected;
 var json_fname = './Climate_change_network.json';
 
 d3.json(json_fname, function(error, data) {
-	NodelinkVisTimeDirected(data);
+	var sel = d3.select("#graph");
+	var vis = new NodelinkVisTimeDirected({element: sel, data: data});
 });
